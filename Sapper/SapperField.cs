58c130b72a289f11f38eeebf92731df96c29f335
DedEgor-creator <korepin.02@gmail.com>
2022-02-11 17:31:42 +0300

    Class "SapperField" ready

---
+++ Sapper/SapperField.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper
+{
+    internal enum Difficulty : byte
+    {
+        Beginner = 10,
+        Amateur = 40,
+        Professional = 99
+    }
+    internal class SapperField
+    {
+        internal sbyte[,] Field { get; }
+        private Random r = new Random();
+        internal SapperField(Difficulty difficulty)
+        {
+            if (difficulty == Difficulty.Beginner)
+                Field = new sbyte[9, 9];
+            else if (difficulty == Difficulty.Amateur)
+                Field = new sbyte[16, 16];
+            else
+                Field = new sbyte[30, 16];
+            int i = 0;
+            int imax = Field.GetLength(0);
+            int jmax = Field.GetLength(1);
+            while (i < (int)difficulty)
+            {
+                if(SetMine(imax,jmax))
+                    i++;
+            }
+        }
+        private bool SetMine(int imax, int jmax)
+        {
+            int idx = r.Next(0, imax);
+            int jdx = r.Next(0, jmax);
+            if (Field[idx, jdx] != -1)
+            {
+                Field[idx, jdx] = -1;
+                #region Increment around values 
+                try
+                {
+                    if (Field[idx - 1, jdx] != -1)
+                        Field[idx - 1, jdx]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx + 1, jdx] != -1)
+                        Field[idx + 1, jdx]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx - 1, jdx - 1] != -1)
+                        Field[idx - 1, jdx - 1]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx + 1, jdx + 1] != -1)
+                        Field[idx + 1, jdx + 1]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx, jdx - 1] != -1)
+                        Field[idx, jdx - 1]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx, jdx + 1] != -1)
+                        Field[idx, jdx + 1]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx - 1, jdx + 1] != -1)
+                        Field[idx - 1, jdx + 1]++;
+                }
+                catch { }
+                try
+                {
+                    if (Field[idx + 1, jdx - 1] != -1)
+                        Field[idx + 1, jdx - 1]++;
+                }
+                catch { }
+                #endregion
+                return true;
+            }
+            return false;
+        }
+    }
+}
