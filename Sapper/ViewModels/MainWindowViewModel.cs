f1c4f3004a8c0859864a1a24e3b8229a350927ad
DedEgor-creator <korepin.02@gmail.com>
2022-02-12 16:59:32 +0300

    The game design is ready

--- Sapper/ViewModels/MainWindowViewModel.cs
+++ Sapper/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 using Sapper.ViewModels.Base;
 using System.Windows.Input;
 using Sapper.Infrastructure.Commands;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Sapper.ViewModels
 {
@@ -27,6 +29,56 @@ namespace Sapper.ViewModels
         private void OnCloseAppCommandExecuted(object p)
         {
             Application.Current.Shutdown();
+            //Label label = new Label();
+            //label.Content = sapper.Field[i, j].ToString();
+            //label.FontWeight = FontWeights.Bold;
+            //label.HorizontalContentAlignment = HorizontalAlignment.Center;
+            //label.VerticalContentAlignment = VerticalAlignment.Center;
+            //label.FontSize = 20;
+            //label.Foreground = new SolidColorBrush(Color.FromArgb(255, 24, 29, 237));
+            //label.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 68, 64, 71));
+            //label.BorderThickness = new Thickness(0.5);
+            //label.Background = new SolidColorBrush(Color.FromArgb(255, 197, 197, 204));
+            //if (sapper.Field[i, j] == -1)
+            //{
+            //    Image img = new Image();
+            //    img.Source = BitmapFrame.Create(new Uri(@"C:\Desktop\Work_VS\MyApps\Sapper\Sapper\Data\mine.png"));
+            //    label.Content = img;
+            //}
+            //else if (sapper.Field[i, j] == 0)
+            //{
+            //    label.Content = null;
+            //}
+            //else if (sapper.Field[i, j] == 1)
+            //{
+            //    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 24, 29, 237));
+            //}
+            //else if (sapper.Field[i, j] == 2)
+            //{
+            //    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 62, 151, 30));
+            //}
+            //else if (sapper.Field[i, j] == 3)
+            //{
+            //    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 215, 22, 50));
+            //}
+            //else if (sapper.Field[i, j] == 4)
+            //{
+            //    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 22, 41, 164));
+            //}
+            //else if (sapper.Field[i, j] == 5)
+            //{
+            //    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 138, 12, 12));
+            //}
+            //Grid.SetRow(label, i);
+            //Grid.SetColumn(label, j);
+            //grid.Children.Add(label);
+            //Button bt = new Button();
+            //bt.Background = new SolidColorBrush(Color.FromArgb(255, 184, 184, 190));
+            //bt.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 68, 64, 71));
+            //Grid.SetRow(bt, i);
+            //Grid.SetColumn(bt, j);
+            //bt.Click += Bt_Click;
+            //grid.Children.Add(bt);
         }
 
         #endregion
--- TestWpf/MainWindow.xaml.cs
+++ TestWpf/MainWindow.xaml.cs
@@ -29,7 +29,6 @@ namespace TestWpf
         {
             SapperField sapper = new SapperField(Difficulty.Beginner);
             Grid grid = new Grid();
-            grid.ShowGridLines = true;
             for (int i = 0; i < 9; i++)
             {
                 grid.RowDefinitions.Add(new RowDefinition());
@@ -42,13 +41,53 @@ namespace TestWpf
                 {
                     Label label = new Label();
                     label.Content = sapper.Field[i, j].ToString();
+                    label.FontWeight = FontWeights.Bold;
+                    label.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    label.VerticalContentAlignment = VerticalAlignment.Center;
+                    label.FontSize = 20;
+                    label.Foreground = new SolidColorBrush(Color.FromArgb(255, 24, 29, 237));
+                    label.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 68, 64, 71));
+                    label.BorderThickness = new Thickness(0.5);
+                    label.Background = new SolidColorBrush(Color.FromArgb(255, 197, 197, 204));
+                    if (sapper.Field[i, j] == -1)
+                    {
+                        Image img = new Image();
+                        img.Source = BitmapFrame.Create(new Uri(@"C:\Desktop\Work_VS\MyApps\Sapper\Sapper\Data\mine.png"));
+                        label.Content = img;
+                    }
+                    else if (sapper.Field[i, j] == 0)
+                    {
+                        label.Content = null;
+                    }
+                    else if (sapper.Field[i, j] == 1)
+                    {
+                        label.Foreground = new SolidColorBrush(Color.FromArgb(255, 24, 29, 237));
+                    }
+                    else if (sapper.Field[i, j] == 2)
+                    {
+                        label.Foreground = new SolidColorBrush(Color.FromArgb(255, 62, 151, 30));
+                    }
+                    else if (sapper.Field[i, j] == 3)
+                    {
+                        label.Foreground = new SolidColorBrush(Color.FromArgb(255, 215, 22, 50));
+                    }
+                    else if (sapper.Field[i, j] == 4)
+                    {
+                        label.Foreground = new SolidColorBrush(Color.FromArgb(255, 22, 41, 164));
+                    }
+                    else if (sapper.Field[i, j] == 5)
+                    {
+                        label.Foreground = new SolidColorBrush(Color.FromArgb(255, 138, 12, 12));
+                    }
                     Grid.SetRow(label, i);
                     Grid.SetColumn(label, j);
+                    grid.Children.Add(label);
                     Button bt = new Button();
+                    bt.Background = new SolidColorBrush(Color.FromArgb(255, 184, 184, 190));
+                    bt.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 68, 64, 71));
                     Grid.SetRow(bt, i);
                     Grid.SetColumn(bt, j);
                     bt.Click += Bt_Click;
-                    grid.Children.Add(label);
                     grid.Children.Add(bt);
                 }
             }
